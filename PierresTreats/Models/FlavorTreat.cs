77d991930b4ae3855df631d5aef499c598778bdc
Jeff Lai <pjefflai@gmail.com>
2022-01-14 09:06:28 -0800

    add all models files

---
+++ PierresTreats/Models/FlavorTreat.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace PierresTreats.Models
+{
+  public class FlavorTreat
+  {
+    public int FlavorTreatId { get; set; }
+    public int FlavorId { get; set; }
+    public int TreatId { get; set; }
+    public virtual Flavor Flavor { get; set; }
+    public virtual Treat Treat { get; set; }
+  }
+}
